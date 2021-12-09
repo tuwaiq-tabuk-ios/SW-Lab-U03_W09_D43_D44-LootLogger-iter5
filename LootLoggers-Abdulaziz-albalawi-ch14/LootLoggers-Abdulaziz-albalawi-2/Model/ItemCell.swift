a89926938fdd9c2274902ef8c8227b2a3e15967d
عبدالعزيز البلوي <bdalzyzalblwy@MacBook-Air-bdalzyz.local>
2021-12-09 09:16:18 +0300

    iter5

---
+++ LootLoggers-Abdulaziz-albalawi-ch14/LootLoggers-Abdulaziz-albalawi-2/Model/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  LootLoggers
+//
+//  Created by عبدالعزيز البلوي on 17/04/1443 AH.
+//
+
+
+import UIKit
+class ItemCell: UITableViewCell {
+  @IBOutlet var nameLabel: UILabel!
+      @IBOutlet var serialNumberLabel: UILabel!
+      @IBOutlet var valueLabel: UILabel!
+}
+

f6a69c632208d885bf092f632ef6d0e260be93d4
Yousef Albalawi <yousef@Yousefs-MacBook-Air.local>
2021-12-09 13:37:44 +0300

    LootLogger-iter5

---
+++ LootLogger_C15_Yousef Al-balwi/LootLogger_C10_Yousef-Al-balwi/ItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Yousef Albalawi on 17/04/1443 AH.
+//
+
+import UIKit
+class ItemCell: UITableViewCell {
+
+  
+  
+  
+  @IBOutlet var nameLabel: UILabel!
+ 
+  @IBOutlet var serialNumberLabel: UILabel!
+  
+  @IBOutlet var valueLabel: UILabel!
+  
+
+} 

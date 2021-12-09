16c29892a4e8dd00ba15feb7549864e2c2eec886
arwabalawi <arwa62885@gmail.com>
2021-12-09 09:05:17 +0300

    completed

---
+++ LootLogger  16 /LootLogger/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by arwa balawi on 19/04/1443 AH.
+//
+
+import Foundation
+
+import UIKit
+ 
+class ItemCell: UITableViewCell {
+  
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var serialNumberLabel: UILabel!
+  @IBOutlet var valueLabel: UILabel!
+   
+  
+}

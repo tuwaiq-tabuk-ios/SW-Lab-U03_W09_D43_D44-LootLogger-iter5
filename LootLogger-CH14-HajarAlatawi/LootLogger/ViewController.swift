8b2494fd8117a83b1160a4c4e34a7afd94a5e2b3
HAJAR <hajar@HAJARs-MacBook-Pro.local>
2021-12-09 09:03:40 +0300

    Completed LootLogger-iter5

---
+++ LootLogger-CH14-HajarAlatawi/LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by HAJAR on 14/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
